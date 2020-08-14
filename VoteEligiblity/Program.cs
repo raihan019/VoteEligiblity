e80225c9b3eacc3c5f18a46e52ee784df00f32f3
unknown <raihan.emailbox@gmail.com>
2020-08-14 17:54:01 +0600

    switch case

--- VoteEligiblity/Program.cs
+++ VoteEligiblity/Program.cs
@@ -10,26 +10,22 @@ namespace VoteEligiblity
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please Input 1st number") ; 
-            decimal number1 = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Please Input a day number") ; 
+            string number1 = Console.ReadLine();
 
-            Console.WriteLine("Please Input 2nd number");
-            decimal number2 = Convert.ToDecimal(Console.ReadLine());
+            switch (number1)
+            {
+                case "1":
+                    Console.WriteLine("Saturday");
+                    break;
+                case "2":
+                    Console.WriteLine("Sun Day");
+                    break;
+                default:
+                    Console.WriteLine("Invalid day");
+                    break;
+            }
 
-            Console.WriteLine("Please Input 3rd number");
-            decimal number3 = Convert.ToDecimal(Console.ReadLine());
-
-            if (number1 > number2 && number1 > number3)
-                Console.WriteLine("" + number1 + " is maximum than " + number2 + " and " + number3 + "");
-            else if (number2 > number1 && number2 > number3)
-                Console.WriteLine("" + number2 + " is maximum than " + number1 + " and " + number3 + "");
-            else if (number3 > number1 && number3 > number2)
-                Console.WriteLine("" + number3 + " is maximum than " + number1 + " and " + number2 + "");
-            else
-                Console.WriteLine("" + number1 + "," + number2 + " and " + number3 + " are equal number");
-
-
-            Console.WriteLine("Thank you");
             Console.ReadKey();
         }
     }
